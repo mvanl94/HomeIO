a800b7dd690e783824f9abca5a6aa81c80f3d1cd
mvanl94 <martijn_vl@hotmailcom>
2016-12-14 20:08:18 +0100

    Release

---
+++ app.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "SwitchOn":
+        return {}
+    baseurl = "http://86.95.145.13?pin=On"
+
+    result = urllib.urlopen(baseurl).read()
+        
+    res = makeWebhookResult()
+    
+    return res
+
+
+
+def makeWebhookResult():
+
+    speech = "I turned the Christmas tree lights on"
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+app.run(debug=False, port=port, host='0.0.0.0')
